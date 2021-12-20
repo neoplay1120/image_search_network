1bf1c4b98533da4e78b1bd278e89a1a9b1d1da1d
yun <neoplay1120@gmail.com>
2021-12-20 11:08:28 +0900

    네트워크 통신기초2

--- lib/ui/test/test_screen.dart
+++ lib/ui/test/test_screen.dart
@@ -14,14 +14,21 @@ class TestScreen extends StatefulWidget {
 class _TestScreenState extends State<TestScreen> {
   Album? _album;
 
-///sync 그리고 Async
+  Future<void> init() async {
+    Album album = await fetchAlbum();
+    setState(() {
+      _album = album;
+    });
+  }
+
+  ///sync 그리고 Async
   /// 동기(Sync) , 비동기(Async)
   /// Future 클래스 비동기식
   /// 미래에 <http.Resoponse>
   /// import 'package:http/http.dart' as http; <<<별명 http붙여도 되냐 안되냐!
   /// 탑레벨 사용하는 녀석들 가독성 때문에
   /// 퓨처는 리스폰 미래에 결과를 받을 것이고 언제가 될지 모른다.
-  //
+  //오래 걸리는 처
   Future<Album> fetchAlbum() async {
     /// await [Future]가 리턴되는 코드]
     /// async + await 는 세트
@@ -30,8 +37,7 @@ class _TestScreenState extends State<TestScreen> {
 
     ///상태코드
     if (response.statusCode == 200) {
-      // If the server did return a 200 OK response,
-      // then parse the JSON.
+
       ///JsonDecode
       return Album.fromJson(jsonDecode(response.body));
     } else {
@@ -40,7 +46,6 @@ class _TestScreenState extends State<TestScreen> {
       /// Exception
       throw Exception('Failed to load album');
     }
-
   }
 
   @override
@@ -48,21 +53,66 @@ class _TestScreenState extends State<TestScreen> {
     // TODO: implement initState
     super.initState();
 
-    fetchAlbum().then((album) {
-        setState(() {
-          _album = album;
-        });
-    });
+    init();
+
+    // init()사용하는 것과 동일하게 된다.
+    // fetchAlbum().then((album) {
+    //     setState(() {
+    //       _album = album;
+    //     });
+    // });
   }
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-          appBar: AppBar(
-            title: const Text('Network Sample'),
-          ),
-          body: Text(_album.toString()),
-        );
+      appBar: AppBar(
+        title: const Text('Network Sample'),
+      ),
+      body: FutureBuilder<Album>(
+          future: fetchAlbum(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: Text('네트워크 에러!.'));
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            // 데이터가 없다면
+            if (!snapshot.hasData) {
+              return const Center(child: Text('ㄷㅔ이터가 없습니다.'));
+            }
+
+            // 데이터가 여기에서는 무조건 있는 상황
+            final Album album = snapshot.data!;
+
+            return Text(
+                '${album.id} : ${album.toString()}',
+                style: const TextStyle(fontSize: 30)
+            );
+          }
+      ),
+      //앨법이 NULL이라면
+      //   body: _album == null
+      //       ? Center(child: const CircularProgressIndicator())
+      //       : Container(
+      //       child: Text(
+      //         // '${_album.id} : ${_album.toString()}',
+      //           _album.toString(),
+      //       style: const TextStyle(fontSize:30),
+      //   )
+      // ),
+
+    );
+  }
+
+  Widget buildBody(Album album) {
+    return Text(
+        '${album.id} : ${album.toString()}',
+        style: const TextStyle(fontSize: 30)
+    );
   }
 }
+
